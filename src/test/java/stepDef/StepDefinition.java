edf525d2b22e71f306b08db406d54edbb84ab91d
naveenreddy9999 <naveenreddybandi123@gmail.com>
2025-03-02 18:09:36 +0530

    new

--- src/test/java/stepDef/StepDefinition.java
+++ src/test/java/stepDef/StepDefinition.java
@@ -1,11 +1,17 @@
 package stepDef;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class StepDefinition {
 
     @Given("User navigating to the login page")
     public void user_navigating_to_the_login_page() {
           System.out.println("hello");
+    }
+
+    @When("log in {string}")
+    public void logIn(String arg0) {
+        System.out.println(arg0);
     }
 }
--- src/test/resources/feature/loginfeature.feature
+++ src/test/resources/feature/loginfeature.feature
@@ -3,3 +3,16 @@ Feature: login
   @login
   Scenario: user login
     Given User navigating to the login page
+    When log in "log"
+
+
+  @login1
+  Scenario: user login
+    Given User navigating to the login page
+    When log in "log1"
+
+
+  @login2
+  Scenario: user login
+    Given User navigating to the login page
+    When log in "log2"
