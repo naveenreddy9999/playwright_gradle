4266f818156cc66c20b2e7a8b38ec00774cc5309
naveenreddy9999 <naveenreddybandi123@gmail.com>
2025-04-05 18:11:06 +0530

    command change+

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,6 +1,5 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = {"classpath:feature"},
--- src/test/java/stepDef/StepDefinition.java
+++ src/test/java/stepDef/StepDefinition.java
@@ -4,16 +4,32 @@ import com.microsoft.playwright.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 public class StepDefinition {
 
     @Given("User navigating to the login page")
-    public void user_navigating_to_the_login_page() {
+    public void user_navigating_to_the_login_page() throws InterruptedException {
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(true));
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
         BrowserContext browserContext = browser.newContext();
         Page page = browserContext.newPage();
         page.navigate("https://mvnrepository.com/artifact/com.microsoft.playwright/playwright/1.51.0",new Page.NavigateOptions().setTimeout(50000));
+        Thread.sleep(50000);
+        File folder = new File("screenshots");
+        if (!folder.exists()) {
+            folder.mkdirs();  // Create the directory if it doesn't exist
+        }
+
+        // Take the screenshot and save it in the folder
+        String screenshotPath = folder.getAbsolutePath() + "/screenshot.png";
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(screenshotPath)).setFullPage(true));
+
+        System.out.println("Screenshot saved at: " + screenshotPath);
+        page.screenshot(new Page.ScreenshotOptions().setFullPage(true));
         System.out.println("title :: "+page.title());
+        playwright.close();
     }
 
     @When("log in {string}")
